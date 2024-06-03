bd60e3fa45dec415b8fe1a1e8950c17f482a70a8
syihwamoza <mozakastella26@gmail.com>
2024-06-03 09:55:00 +0700

    unlimited loop untuk menulis

--- StreammReaderandWriter2_0111/StreammReaderandWriter2_0111.cpp
+++ StreammReaderandWriter2_0111/StreammReaderandWriter2_0111.cpp
@@ -1,14 +1,20 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main()
 {
     string baris;
 
-    ofstream outfile;
+   ofstream outfile;
     outfile.open("contohfile.txt");
 
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+    while (true) {
+
+    }
 }
 
 
