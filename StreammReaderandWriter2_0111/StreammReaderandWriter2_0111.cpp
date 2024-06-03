190bed5f9bdb47891176a1a2740a7f42de0c6b13
syihwamoza <mozakastella26@gmail.com>
2024-06-03 10:00:44 +0700

    melakukan perulangan setiap baris

--- StreammReaderandWriter2_0111/StreammReaderandWriter2_0111.cpp
+++ StreammReaderandWriter2_0111/StreammReaderandWriter2_0111.cpp
@@ -26,7 +26,10 @@ int main()
     cout << endl << ">= Membuka dan membaca file " << endl;
     if (infile.is_open())
     {
+        while (getline(infile, baris))
+        {
 
+        }
     }
 }
 
