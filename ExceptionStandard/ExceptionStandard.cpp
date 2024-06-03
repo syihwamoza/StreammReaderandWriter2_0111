e389699b0e7dd2d70fa237d654dd6dab5cad7889
syihwamoza <mozakastella26@gmail.com>
2024-06-03 10:25:46 +0700

    untuk obyek  exception yang aakn digunakan

---
+++ ExceptionStandard/ExceptionStandard.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <exception>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
--- iosfilewithpad(2)/iosfilewithpad(2).cpp
+++ iosfilewithpad(2)/iosfilewithpad(2).cpp
@@ -17,7 +17,27 @@ int main()
 	cout << ">= Menulis file, \'q\' utuk keluar" << endl;
 
 	while (true) {
-
+		cout << "- ";
+		getline(cin, baris);
+		if (baris == "q") break;
+		outfile << baris << endl;
 	}
+	outfile.close();
+
+	ifstream infile;
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuk adan membaca file" << endl;
+	if (infile.is_open())
+	{
+		while (getline(infile, baris))
+		{
+			cout << baris << \'n';
+		}
+
+		infile.close();
+	}
+	else cout << "Unable to open file";
+	return 0;
 }
 
